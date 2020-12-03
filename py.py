901a538c6be95e9f3eb6d29759365a8d8917988a
Metz <shamin.michael@gmail.com>
2020-12-03 10:18:07 +0300

    03.12

--- py.py
+++ py.py
@@ -1,2 +1,14 @@
-pp = 10*100 + 200*20 + 300*30
-print(pp)
+def say_hello(time, name=''):
+    if time <= 5 or time >= 1:
+        messages = 'Доброй ночи'
+    elif time <= 6 or time >= 10:
+        messages = 'Доброе утро'
+    else:
+        messages = 'Который час?'
+    if name != '':
+        print(messages + ', ' + name + '!')
+    else:
+        print(messages + '!')
+
+say_hello(2)
+say_hello(123, 'Mike')
