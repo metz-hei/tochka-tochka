85441bcda3d5cdaf59a373722dc97c8b38c3af4e
MS <metzhei@yandex.ru>
2021-03-06 12:43:02 +0300

    06.03

--- py.py
+++
@@ -1,9 +0,0 @@
-import random as r
-
-answers = ['Норм.', 'Лучше всех :)', 'Ну так', 'Отличненько!', 'Ничего, жить буду']
-
-
-def how_are_you():
-    return r.choice(answers)
-
-print(how_are_you())
